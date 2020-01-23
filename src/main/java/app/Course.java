7338998b13bdb46976c31d20f3f55664425cecd4
NooraKalpio <noora.kalpio@gmail.com>
2020-01-23 11:36:23 +0200

    pom, course, Tiedostonlukija added

--- src/main/java/app/Course.java
+++ src/main/java/app/Course.java
@@ -1,4 +1,110 @@
 package app;
 
+import java.time.LocalDate;
+
 public class Course {
+        private int courseCode;
+        private String name;
+        private LocalDate startDate;
+        private LocalDate endDate;
+        private String location;
+        private String materialType;
+        private String description;
+        private boolean materialCode;
+        private String mainCategory;
+        private String subCategory1;
+        private String subCategory2;
+
+
+    public Course() {
+
+    }
+
+    public int getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(int courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMaterialType() {
+        return materialType;
+    }
+
+    public void setMaterialType(String materialType) {
+        this.materialType = materialType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isMaterialCode() {
+        return materialCode;
+    }
+
+    public void setMaterialCode(boolean materialCode) {
+        this.materialCode = materialCode;
+    }
+
+    public String getMainCategory() {
+        return mainCategory;
+    }
+
+    public void setMainCategory(String mainCategory) {
+        this.mainCategory = mainCategory;
+    }
+
+    public String getSubCategory1() {
+        return subCategory1;
+    }
+
+    public void setSubCategory1(String subCategory1) {
+        this.subCategory1 = subCategory1;
+    }
+
+    public String getSubCategory2() {
+        return subCategory2;
+    }
+
+    public void setSubCategory2(String subCategory2) {
+        this.subCategory2 = subCategory2;
+    }
 }
--- src/main/java/app/Tiedostonlukija.java
+++ src/main/java/app/Tiedostonlukija.java
@@ -1,4 +1,31 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Tiedostonlukija {
+
+    public Tiedostonlukija() {
+
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("CourseData_20180901_210145.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        }
+
+        List<List<String>> records = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File("book.csv"));) {
+            while (scanner.hasNextLine()) {
+                records.add(getRecordFromLine(scanner.nextLine()));
+            }
+        }
+
+    }
 }
